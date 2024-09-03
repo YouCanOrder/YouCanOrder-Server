8b1913717b7c4f0e44bcb60abff7858c875b016c
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-03 16:07:47 +0900

    chore: initial commit

---
+++ src/test/java/site/youcanorder/YouCanOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package site.youcanorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouCanOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
