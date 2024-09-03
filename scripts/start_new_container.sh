968ced7bc377113b16fd1b8c406f53ad5618d3e1
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-03 23:38:14 +0900

    ci: remove image pull statements in `start_new_container.sh`

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -2,9 +2,5 @@
 
 DOCKER_TAG=$(aws ssm get-parameter --name "/youcanorder/DOCKER_TAG" --query "Parameter.Value" --output text)
 
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_URI"
-
-docker pull "$DOCKER_TAG":latest
-
 docker run -d --name youcanorder-server \
   "DOCKER_TAG":latest
