5d289b4b656d8c92fae7ef8420f07407bc2a0151
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-09-03 23:37:50 +0900

    ci: remove unnecessary parameters in `start_new_container.sh`

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-AWS_REGION=$(aws ssm get-parameter --name "/youcanorder/AWS_REGION" --query "Parameter.Value" --output text)
-ECR_URI=$(aws ssm get-parameter --name "/youcanorder/ECR_URI" --query "Parameter.Value" --output text)
 DOCKER_TAG=$(aws ssm get-parameter --name "/youcanorder/DOCKER_TAG" --query "Parameter.Value" --output text)
 
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_URI"
